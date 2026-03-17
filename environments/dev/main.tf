203d37938f6b4712594da7315a9b10abad50bc0e
Prince Agyei <ybktalian@gmail.com>
2026-03-17 02:30:30 +0000

    defines vpc resources

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,24 +2,81 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "6.6.0"
+resource "aws_vpc" "dev_vpc" {
+  cidr_block = "10.10.10.0/25"
+  assign_generated_ipv6_cidr_block = true # Requests /56 from Amazon
 
-
-  name = "dev_vpc"
-  cidr = "10.10.10.0/25"
-
-  azs             = ["eu-west-2a"]
-  private_subnets = ["10.10.10.0/27", "10.10.10.32/27"]
-  public_subnets  = ["10.10.10.64/27", "10.10.10.96/27"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway = true
-  enable_vpn_gateway   = false
-  enable_dns_hostnames = true
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
+   tags = {
+    Name = "main.vpc"
   }
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.10.10.64/27"
+  ipv6_cidr_block = cidrsubnet(aws_vpc.dev_vpc.ipv6_cidr_block)
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "public_subnet_1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.10.10.96/27"
+  ipv6_cidr_block = cidrsubnet(aws_vpc.dev_vpc.ipv6_cidr_block)
+  availability_zone = "eu-west-2b"
+  tags = {
+    Name = "public_subnet_2"
+  }
+}
+
+resource "aws_internet_gateway" "dual_stack_igw" {
+  vpc_id = aws_vpc.dev_vpc.id
+  tags = {
+    Name = "vpc_ipv4_igw"
+  } 
+}
+resource "aws_route_table" "dual_stack_route_table" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0" #all routes to through the gateway
+    gateway_id = aws_internet_gateway.dual_stack_igw.id
+  }
+  route {
+    ipv6_cidr_block = "::/0" #all routes to through the gateway
+    gateway_id = aws_internet_gateway.dual_stack_igw.id
+  }
+  tags = {
+    Name = "public dual stack route table"
+  }
+}
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.dual_stack_route_table.id
+}
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.dual_stack_route_table.id
+}
+
+# resource "aws_route_table" "subnet1_route_table" {
+#   vpc_id = aws_vpc.dev_vpc.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
+
+#   tags = {
+#     Name = "Subnet 1 route table"
+#   }
+# }
+# resource "aws_egress_only_internet_gateway" "ipv6_igw" {
+#   vpc_id = aws_vpc.dev_vpc.id
+#   tags = {
+#     Name = "vpc_ipv6_igw"
+#   }
+# }
