6bb9ca7c424ad41bbabfaf9ee4d530eaf407eeed
Prince Agyei <ybktalian@gmail.com>
2026-04-05 00:56:37 +0100

    added ec2 instances in private subnets, defined outputs

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,1 +1,27 @@
-
+output "vpc_id" {
+    value = aws_vpc.dev_vpc.id
+}
+output "vpv_ipv4_cidr" {
+    value = aws_vpc.dev_vpc.cidr_block
+}
+output "vpv_ipv6_cidr" {
+    value = aws_vpc.dev_vpc.ipv6_cidr_block
+}
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+output "APP_SERVER_A" {
+    value = aws_instance.app-serve_a.private_ip  
+}
+output "APP_SERVER_A" {
+    value = aws_instance.app_server_a.ipv6_addresses[0]
+}
+output "APP_SERVER_B" {
+    value = aws_instance.app-serve_b.private_ip  
+}
+output "APP_SERVER_B" {
+    value = aws_instance.app_server_b.ipv6_addresses[0]
+}
