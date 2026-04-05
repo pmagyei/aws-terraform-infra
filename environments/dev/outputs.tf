be86a0d339f16fe0a4c238cf910b956ba05848e7
Prince Agyei <ybktalian@gmail.com>
2026-04-05 01:06:51 +0100

    syntax update

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -8,20 +8,26 @@ output "vpv_ipv6_cidr" {
     value = aws_vpc.dev_vpc.ipv6_cidr_block
 }
 output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
+  value = [
+    aws_subnet.public_subnet_a.id,
+    aws_subnet.public_subnet_b.id
+  ]
 }
 output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
+  value = [
+    aws_subnet.private_subnet_a.id,
+    aws_subnet.private_subnet_b.id
+  ]
 }
-output "APP_SERVER_A_ipv4" {
+output "app_server_a_private_ipv4" {
     value = aws_instance.app-serve_a.private_ip  
 }
-output "APP_SERVER_A_ipv6" {
+output "app_server_a_private_ipv6" {
     value = aws_instance.app_server_a.ipv6_addresses[0]
 }
-output "APP_SERVER_B_ipv4" {
+output "app_server_b_private_ipv4" {
     value = aws_instance.app-serve_b.private_ip  
 }
-output "APP_SERVER_B_ipv6" {
+output "app_server_b_private_ipv4" {
     value = aws_instance.app_server_b.ipv6_addresses[0]
 }
