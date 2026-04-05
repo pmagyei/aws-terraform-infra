53610177632d2e5ec04317e2b6cf68e1738a6e3c
Prince Agyei <ybktalian@gmail.com>
2026-04-05 01:00:04 +0100

    syntax update

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -13,15 +13,15 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
-output "APP_SERVER_A" {
+output "APP_SERVER_A_ipv4" {
     value = aws_instance.app-serve_a.private_ip  
 }
-output "APP_SERVER_A" {
+output "APP_SERVER_A_ipv6" {
     value = aws_instance.app_server_a.ipv6_addresses[0]
 }
-output "APP_SERVER_B" {
+output "APP_SERVER_B_ipv4" {
     value = aws_instance.app-serve_b.private_ip  
 }
-output "APP_SERVER_B" {
+output "APP_SERVER_B_ipv6" {
     value = aws_instance.app_server_b.ipv6_addresses[0]
 }
